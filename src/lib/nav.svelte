e47316c6248e4552f597a2591ad30d993cef6261
Jimmy De Guzman <jimmy.c.de.guzman@icloud.com>
2022-01-29 22:33:24 +0800

    feat: :sparkles: add routes and pages

---
+++ src/lib/nav.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let pages
+</script>
+
+<div
+  class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box"
+>
+  <div class="px-2 mx-2 navbar-start">
+    <a href="/">
+      <span class="text-lg font-bold"> JCD Blog </span>
+    </a>
+  </div>
+  <div class="px-2 mx-2 navbar-center">
+    <div class="flex items-stretch">
+      <a href="/" class="btn btn-ghost btn-sm rounded-btn">Home</a>
+      {#each pages as { title, slug }}
+        <a href={`/${slug}`} class="btn btn-ghost btn-sm rounded-btn"
+          >{title}</a
+        >
+      {/each}
+    </div>
+  </div>
+  <div class="navbar-end" />
+</div>
